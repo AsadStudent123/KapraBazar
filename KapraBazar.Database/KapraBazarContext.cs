7594645e9a1da9e6e9d1d49cdd82beeb82047929
Asad Ali <asad201387@gmail.com>
2021-01-16 19:28:48 +0500

    Product n Category Entitles with enabling migration

---
+++ KapraBazar.Database/KapraBazarContext.cs
@@ -0,0 +1,20 @@
+﻿using KapraBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KapraBazar.Database
+{
+    public class KapraBazarContext : DbContext
+    {
+        public KapraBazarContext() :base("KapraBazarConnection")
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
